0a880fe7c4989e5c93541a3e6967cc1e3ef514cf
Clinton Brito <clintoncsb@gmail.com>
2023-09-25 15:26:16 -0300

    🚨 fix: linter issues

--- src/middlewares/filterByNameRateAndDate.js
+++ src/middlewares/filterByNameRateAndDate.js
@@ -1,32 +1,32 @@
-const { readFile } = require('../helpers/utils');
-const filterTalkersByName = require('./filterTalkersByName');
-// const filterTalkersByDate = require('./filterTalkersByDate');
+// const { readFile } = require('../helpers/utils');
+// const filterTalkersByName = require('./filterTalkersByName');
+// // const filterTalkersByDate = require('./filterTalkersByDate');
 
-const HTTP_OK_STATUS = 200;
+// const HTTP_OK_STATUS = 200;
 
-// eslint-disable-next-line max-lines-per-function, complexity
-const filterByNameRateAndDate = async (req, res, next) => {
-  const { rate, q, date } = req.query;
-  const rateNumber = Number(rate);
-  const talkers = await readFile();
+// // eslint-disable-next-line max-lines-per-function, complexity
+// const filterByNameRateAndDate = async (req, res, next) => {
+//   const { rate, q, date } = req.query;
+//   const rateNumber = Number(rate);
+//   const talkers = await readFile();
 
-  const filteredByName = filterTalkersByName(talkers, q);
-  // const filteredByDate = filterTalkersByDate(talkers, date);
+//   const filteredByName = filterTalkersByName(talkers, q);
+//   // const filteredByDate = filterTalkersByDate(talkers, date);
 
-  if (q && rateNumber && date) {
-    const filteredTalkersByName = filteredByName
-      .filter((talker) => talker.talk.rate === +rate);
+//   if (q && rateNumber && date) {
+//     const filteredTalkersByName = filteredByName
+//       .filter((talker) => talker.talk.rate === +rate);
 
-    const filteredTalkersByDate = filteredTalkersByName
-      .filter((talker) => talker.talk.watchedAt === date);
+//     const filteredTalkersByDate = filteredTalkersByName
+//       .filter((talker) => talker.talk.watchedAt === date);
 
-    if (!filteredTalkersByDate.length || !filteredTalkersByName.length) {
-      return res.status(HTTP_OK_STATUS).json([]);
-    }
+//     if (!filteredTalkersByDate.length || !filteredTalkersByName.length) {
+//       return res.status(HTTP_OK_STATUS).json([]);
+//     }
 
-    return res.status(HTTP_OK_STATUS).json(filteredTalkersByName);
-  }
-  next();
-};
+//     return res.status(HTTP_OK_STATUS).json(filteredTalkersByName);
+//   }
+//   next();
+// };
 
-module.exports = filterByNameRateAndDate;
+// module.exports = filterByNameRateAndDate;
